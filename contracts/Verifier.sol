4d56a047343691b19b4a9d14ec8f8eb3fa0b3f15
osuke <arget-fee.free.dgm@hotmail.co.jp>
2018-11-13 11:24:41 +0900

    Add structs and parameters

--- contracts/Verifier.sol
+++ contracts/Verifier.sol
@@ -1,15 +1,41 @@
 pragma solidity ^0.4.24;
-
+pragma experimental ABIEncoderV2;
 
 contract VerifierContract {
 
+    struct Proof {
+        bytes32 root;               // merkle root of P, D and B - evaluations 
+        bytes32 lRoot;              // merkle root of L - evaluations
+        bytes[] branches;           // branches of P, D and B - evaluations
+        FriComponent friComponent;  // low-degree proofs
+    }
+
+    struct FriComponent {
+        bytes32 root;      // merkle root of columns
+        bytes[] branches;  // branches of the column and the four values in the polynominal
+    }
+
+
     // verify an FRI proof
-    function verifyLowDegreeProof() public returns (bool) {
+    function verifyLowDegreeProof(
+        bytes32 merkleRoot, 
+        uint rootOfUnity, 
+        FriComponent[] friComponents, 
+        uint maxDegPlus1, 
+        uint modulus, 
+        uint excludeMultiplesOf
+    ) public returns (bool) {
         return false;
     }
 
     // verify a STARK
-    function verifyMimcProof() public returns (bool) {
+    function verifyMimcProof(
+        uint input, 
+        uint steps, 
+        uint roundConstants, 
+        uint output, 
+        Proof proof
+    ) public returns (bool) {
         return false;
     }
 }
