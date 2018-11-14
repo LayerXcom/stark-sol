dac22a9fdf33b2b0c32c1c0b117c6743a5afea1a
osuke <arget-fee.free.dgm@hotmail.co.jp>
2018-11-14 17:05:34 +0900

    seperate two func

--- contracts/Verifier.sol
+++ contracts/Verifier.sol
@@ -14,7 +14,7 @@ contract VerifierContract {
         bytes32 root;               // merkle root of P, D and B - evaluations 
         bytes32 lRoot;              // merkle root of L - evaluations
         bytes[][] branches;           // branches of P, D and B - evaluations
-        FriComponent friComponent;  // low-degree proofs
+        FriComponent[] friComponent;  // low-degree proofs
     }
 
     struct FriComponent {
@@ -50,8 +50,8 @@ contract VerifierContract {
         Proof _proof
     ) public returns (bool) 
     {
-        bytes32 root = _proof.root;
-        bytes32 lRoot = _proof.lRoot;
+        // bytes32 root = _proof.root;
+        // bytes32 lRoot = _proof.lRoot;
         bytes[][] memory branches = _proof.branches;
         FriComponent[] memory friComponent = _proof.friComponent;
 
@@ -59,58 +59,76 @@ contract VerifierContract {
         require(isPowerOf2(_steps) && isPowerOf2(_roundConstants.length));
         require(_roundConstants.length < _steps);
 
-        uint precision = _steps.mul(EXTENSION_FACTOR);
-        uint G2 = (7 ** ((MODULUS - 1).div(precision))) % MODULUS;
-        uint skips = precision.div(_steps);
+        // uint precision = _steps.mul(EXTENSION_FACTOR);
+        uint G2 = (7 ** ((MODULUS - 1).div(_steps.mul(EXTENSION_FACTOR)))) % MODULUS;
+        uint skips = _steps.mul(EXTENSION_FACTOR).div(_steps);
         uint skips2 = _steps.div(_roundConstants.length);
         
         uint[] memory constantsMiniPolynomial = fft(_roundConstants, MODULUS, (G2 ** (EXTENSION_FACTOR * skips2)) % MODULUS, true);
-        require(verifyLowDegreeProof(lRoot, G2, friComponent, _steps * 2, MODULUS, EXTENSION_FACTOR));
+        require(verifyLowDegreeProof(_proof.lRoot, G2, friComponent, _steps * 2, MODULUS, EXTENSION_FACTOR));
 
         // uint k1 = keccak256(abi.encodePacked(root, 0x01)).toUint(0);
         // uint k2 = keccak256(abi.encodePacked(root, 0x02)).toUint(0);
         // uint k3 = keccak256(abi.encodePacked(root, 0x03)).toUint(0);
         // uint k4 = keccak256(abi.encodePacked(root, 0x04)).toUint(0);
 
-        uint k1 = uint(keccak256(abi.encodePacked(root, 0x01)));
-        uint k2 = uint(keccak256(abi.encodePacked(root, 0x02)));
-        uint k3 = uint(keccak256(abi.encodePacked(root, 0x03)));
-        uint k4 = uint(keccak256(abi.encodePacked(root, 0x04)));
+        // uint k1 = uint(keccak256(abi.encodePacked(_proof.root, 0x01)));
+        // uint k2 = uint(keccak256(abi.encodePacked(_proof.root, 0x02)));
+        // uint k3 = uint(keccak256(abi.encodePacked(_proof.root, 0x03)));
+        // uint k4 = uint(keccak256(abi.encodePacked(_proof.root, 0x04)));
 
-        uint[] memory positions = getPseudorandomIndices(lRoot, precision, SPOT_CHECK_SECURITY_FACTOR, EXTENSION_FACTOR);
+        uint[] memory positions = getPseudorandomIndices(_proof.lRoot, _steps.mul(EXTENSION_FACTOR), SPOT_CHECK_SECURITY_FACTOR, EXTENSION_FACTOR);
         uint lastStepPosition = (G2 ** ((_steps - 1).mul(skips))) % MODULUS;
 
         for (uint i; i < positions.length; i++) {
-            uint x = (G2 ** positions[i]) % MODULUS;
-            uint xToTheSteps = (x ** _steps) % MODULUS;
-
-            // a branch check for P, D and B
-            bytes memory mBranch1 = root.verifyBranch(positions[i], branches[i * 3]);
-            // a branch check for P of g1x
-            bytes memory mBranch2 = root.verifyBranch((positions[i].add(skips)) % precision, branches[i * 3 + 1]);
-            // a branch check for L
-            uint lx = uint(root.verifyBranch(positions[i], branches[i * 3 + 2]));
-
-            uint px = mBranch1.slice(0, 32).toUint(0);
-            uint pG1x = mBranch2.slice(0, 32).toUint(0);
-            uint dx = mBranch1.slice(32, 32).toUint(0);
-            uint bx = mBranch2.slice(64, 32).toUint(0);
-
-            uint zValue = polyDiv((x ** _steps) % MODULUS - 1, x - lastStepPosition);
-            uint kx = evalPolyAt(constantsMiniPolynomial, (x ** skips2) % MODULUS);
-
-            // Check transition constraints C(P(x)) = Z(x) * D(x)
-            require((pG1x - px ** 3 - kx - zValue * dx) % MODULUS == 0);
-
-            // Check boundary constraints B(x) * Q(x) + I(x) = P(x)
-            uint[] memory interpolant = lagrangeInterp2([1, lastStepPosition], [_input, _output]);
-            uint[] memory zeropoly2 = mulPolys([uint(-1), 1], [-lastStepPosition, 1]);
-            require((px - bx * evalPolyAt(zeropoly2, x) - evalPolyAt(interpolant, x)) % MODULUS == 0);
-
-            // Check correctness of the linear combination
-            require((lx - dx - k1 * px - k2 * px * xToTheSteps - k3 * bx - k4 * bx * xToTheSteps) % MODULUS == 0);
+            require(checkForPositions(G2, i, positions, _steps, branches, skips, _input, _output, _proof, constantsMiniPolynomial, lastStepPosition, skips2));
         }
 
+        return true;
+    }
+
+    function checkForPositions(
+        uint G2, 
+        uint i, 
+        uint[] positions, 
+        uint _steps, 
+        bytes[][] branches, 
+        uint skips, 
+        uint _input, 
+        uint _output, 
+        Proof _proof,
+        uint[] constantsMiniPolynomial,
+        uint lastStepPosition,
+        uint skips2
+    ) internal returns (bool) {
+        uint x = (G2 ** positions[i]) % MODULUS;
+        uint xToTheSteps = (x ** _steps) % MODULUS;
+
+        // a branch check for P, D and B
+        bytes memory mBranch1 = _proof.root.verifyBranch(positions[i], branches[i * 3]);
+        // a branch check for P of g1x
+        bytes memory mBranch2 = _proof.root.verifyBranch((positions[i].add(skips)) % _steps.mul(EXTENSION_FACTOR), branches[i * 3 + 1]);
+        // a branch check for L
+        uint lx = _proof.root.verifyBranch(positions[i], branches[i * 3 + 2]).toUint(0);
+
+        uint px = mBranch1.slice(0, 32).toUint(0);
+        uint pG1x = mBranch2.slice(0, 32).toUint(0);
+        uint dx = mBranch1.slice(32, 32).toUint(0);
+        uint bx = mBranch2.slice(64, 32).toUint(0);
+
+        uint zValue = polyDiv((x ** _steps) % MODULUS - 1, x - lastStepPosition);
+        uint kx = evalPolyAt(constantsMiniPolynomial, (x ** skips2) % MODULUS);
+
+        // Check transition constraints C(P(x)) = Z(x) * D(x)
+        require((pG1x - px ** 3 - kx - zValue * dx) % MODULUS == 0);
+
+        // Check boundary constraints B(x) * Q(x) + I(x) = P(x)
+        uint[] memory interpolant = lagrangeInterp2([1, lastStepPosition], [_input, _output]);
+        uint[] memory zeropoly2 = mulPolys([uint(-1), 1], [-lastStepPosition, 1]);
+        require((px - bx * evalPolyAt(zeropoly2, x) - evalPolyAt(interpolant, x)) % MODULUS == 0);
+
+        // Check correctness of the linear combination
+        require((lx - dx - uint(keccak256(abi.encodePacked(_proof.root, 0x01))) * px - uint(keccak256(abi.encodePacked(_proof.root, 0x02))) * px * xToTheSteps -  uint(keccak256(abi.encodePacked(_proof.root, 0x03))) * bx - uint(keccak256(abi.encodePacked(_proof.root, 0x04))) * bx * xToTheSteps) % MODULUS == 0);
         return true;
     }
 
